52cf9d3383521df52d8ce2742e729f6df623e018
owain lewis <owain@owainlewis.com>
2014-01-31 15:00:47 +0000

    updates config and req

--- app.py
+++ app.py
@@ -5,27 +5,30 @@
 # ===============================================
 
 from flask import Flask, jsonify, render_template
-
 from vms import Session
 import os
 
 app = Flask(__name__)
-HOST = ""
-USER = ""
-PWD  = ""
+
+HOST = os.environ['XEN_HOST']
+USER = os.environ['XEN_USER']
+PWD  = os.environ['XEN_PWD']
 
 session = Session(HOST, USER, PWD)
 
 @app.route("/")
 def home():
-    vms = session.vm_list()
-    return render_template("index.html", vms=vms)
+	vms = session.vm_list()
+	return render_template("index.html", vms=vms)
 
 @app.route("/api/vms")
 def active_vms():
-    vms = session.vm_list()
-    return jsonify(vms=vms)
+	vms = session.vm_list()
+	return jsonify(vms=vms)
 
 if __name__ == "__main__":
-    app.run(debug=True,
-            port=5001)
+	print(HOST)
+	print(USER)
+	print(PWD)
+	app.run(debug=True,
+		port=5001)
